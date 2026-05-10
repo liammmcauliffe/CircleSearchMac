79fcec7959ac42660a64b9da52f0467c7c132d68
Liam McAuliffe <lmmcauliffe1223@gmail.com>
2026-05-09 23:29:40 -0400

    feat: initial Xcode project scaffold

---
+++ CircleSearch/CircleSearch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CircleSearch
+//
+//  Created by Liam McAuliffe on 5/9/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
